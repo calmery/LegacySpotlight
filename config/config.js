3569feffda1641b266ef564189df03a472d27af3
Calmery <leaf0115.lab+io950@gmail.com>
2016-06-30 17:32:30 +0900

    Change stable version

--- config/config.js
+++ config/config.js
@@ -1,26 +1,65 @@
-var user = require( './user' )
+var path    = require( 'path' ),
+    fs      = require( 'fs' )
 
-exports.config = {
-    express : {
-        port           : 3000
+var config = {
+    
+    express: {
+        port: ( function(){
+            var net    = require('net'),
+                server = new net.Server()
+            var port
+            for( ;port = 1000 + Math.floor( Math.random() * 9999 ); )
+                try {
+                    server.listen( port, '127.0.0.1' )
+                    server.close()
+                    break
+                } catch( e ) {
+                    continue
+                }
+            return port
+        } )()
     },
+    
     electron: {
-        app            : {
-            show          : false,
-            width         : 800,
-            height        : 600,
-            center        : true,
-            fullscreen    : false,
-            fullscreenable: false,
-            title         : 'Cyber'
+        app: {
+            show: false,
+            width: 800,
+            height: 600,
+            center: true,
+            fullscreen: false,
+            fullscreenenable: false,
+            title : 'Spotlight Beta'
         },
-        maxListeners   : 5
+        maxListeners: 5
     },
-    twitter : {
-        name           : 'Cyber2016',
-        url            : 'apps.twitter.com/app/12368503',
-        consumer_key   : '2aZzmhzrNsXqjQcDv0z3eB8cQ',
+    
+    twitter: {
+        name: 'SpotlightBeta-Patchworks',
+        app: '12368503',
+        consumer_key: '2aZzmhzrNsXqjQcDv0z3eB8cQ',
         consumer_secret: 'qBHKHCoaHOsfrzkTB5AchtWgpB1wCIuQBc6ZMQlylod5QdAdkk'
     },
-    user    : user.config
+    
+    fn: {
+        isExistFile: function( file ){
+            try {
+                fs.statSync( path.resolve( file ) )
+                return true
+            } catch( error ) {
+                if( error.code === 'ENOENT' )
+                    return false
+            }
+        },
+        fixPath: function( url ){
+            return path.resolve( url )
+        },
+        checkUserAgent: function( request ){
+            return request.headers['user-agent'].indexOf('Electron') != -1
+        }
+    }
+    
 }
+
+/* ----- Exports ----- */
+
+exports.config = config
--- config/user.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-    name: '',
-    twitter: {
-        id                 : '',
-        screen_name        : '',
-        access_token_key   : '',
-        access_token_secret: ''
-    }
-}
