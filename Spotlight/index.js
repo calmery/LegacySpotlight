b4cbac300220a39ce64d0c94be8726d63cd15f85
Marei Kikukawa <contact@calmery.me>
2017-02-11 07:32:22 +0900

    Add static path

--- Spotlight/index.js
+++ Spotlight/index.js
@@ -6,12 +6,16 @@ module.exports = ( yacona ) => {
         viewEngine: 'ejs'
     } )
     
+    console.log( spotlight )
+    
+    spotlight.server.app.use( require( 'express' ).static( utility.fixPath( __dirname, 'static' ) ) )
+    
     spotlight.addRoute( '/', ( request, response ) => {
         response.render( utility.fixPath( __dirname, 'public', 'index.ejs' ), {} )
     } )
     
     /* Ready */
     
-    yacona.createWindow( spotlight.url )
+    yacona.createWindow( spotlight.url, { openDevTools: true } )
     
 }
