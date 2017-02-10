d6803d984dd49ada31aaa8666c6b7613697ca1e6
Marei Kikukawa <contact@calmery.me>
2017-02-11 07:06:10 +0900

    Cleanup

--- static/preload.js
+++
@@ -1,7 +0,0 @@
-var socketio = io()
-
-socketio.on( 'processError', function( error ){
-    if( typeof error.value == 'object' )
-        error.value = JSON.stringify( error.value )
-        alert( 'エラーが発生しました．\n\n' + error.value )
-} )
