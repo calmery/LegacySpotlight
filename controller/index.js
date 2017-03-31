a16d3e3f5b8265157fe448e33f8be0da91922910
calmery <contact@calmery.me>
2017-04-01 03:17:48 +0900

    Change to show window only when authenticated

--- controller/index.js
+++ controller/index.js
@@ -1,15 +1,21 @@
 module.exports = yacona => {
     
     let utility = yacona.moduleLoader( 'utility' )
+    let window
     
     yacona.addRoute( './public' )
-    yacona.createWindow( { setMenu: null, setResizable: false, openDevTools: true } )
+    yacona.createWindow( { setMenu: null, setResizable: false, openDevTools: true } ).then( w => {
+        window = w
+        window.hide()
+    } )
     
     yacona.setSocket( 'connection', ( socket, value ) => {
         let auth = yacona.config.load( 'auth', 'user.yaml' )
         if( auth.status === false ){
             yacona.localAppLoader( utility.fixPath( __dirname, '..', 'auth' ) )
             yacona.kill( 'controller' )
+        } else {
+            window.show()
         }
     } )
     
