0155465b08588b01ecb06a8ee8d20b63be0e02be
calmery <contact@calmery.me>
2017-04-06 02:22:54 +0900

    Fix static path

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const utility = yacona.moduleLoader( 'utility' )
 let app = 'spotlight'
 
 yacona.setPrefix( app )
-yacona.addClientModule( 'logo', './resources/img/icon.ico' )
+yacona.addClientModule( 'logo', utility.fixPath( __dirname, './resources/img/icon.ico' ) )
 
-yacona.addClientModule( 'base', './resources/css/base.css' )
-yacona.addClientModule( 'fonts', './resources/css/fonts.css' )
-yacona.addClientModule( 'quicksand', './resources/css/fonts/Quicksand/Quicksand-Regular.ttf' )
+yacona.addClientModule( 'base', utility.fixPath( __dirname, './resources/css/base.css' ) )
+yacona.addClientModule( 'fonts', utility.fixPath( __dirname, './resources/css/fonts.css' ) )
+yacona.addClientModule( 'quicksand', utility.fixPath( __dirname, './resources/css/fonts/Quicksand/Quicksand-Regular.ttf' ) )
 
-yacona.addClientModule( 'preload', './resources/js/preload.js' )
-yacona.addClientModule( 'bind', './resources/js/bind.js' )
-yacona.addClientModule( 'common', './resources/js/common.js' )
+yacona.addClientModule( 'preload', utility.fixPath( __dirname, './resources/js/preload.js' ) )
+yacona.addClientModule( 'bind', utility.fixPath( __dirname, './resources/js/bind.js' ) )
+yacona.addClientModule( 'common', utility.fixPath( __dirname, './resources/js/common.js' ) )
 
 yacona.localAppLoader( utility.fixPath( __dirname, 'controller' ) )
