aaa04bd21353066e21155fe51554075b6534655d
calmery <contact@calmery.me>
2017-04-09 03:46:42 +0900

    Update controller

--- applications/controller/index.js
+++ applications/controller/index.js
@@ -1,6 +1,20 @@
 module.exports = yacona => {
     
+    let controller
+    
     yacona.addRoute( './public' )
-    yacona.createWindow()
+    yacona.createWindow( {
+        setMinimumSize: { 
+            width: 200, 
+            height: 200 
+        }, 
+        setMaximumSize: { 
+            width: 200, 
+            height: 600 
+        }, 
+        width:200, 
+        setMenu: null, 
+        setResizable: true 
+    } ).then( window => controller = window )
     
 }
