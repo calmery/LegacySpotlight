214f82e753b3fd76bd627ec03c84d7d169495e02
calmery <contact@calmery.me>
2017-04-09 03:46:42 +0900

    Update controller

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const yacona = require( 'yacona' )
 
 yacona.setPrefix( 'spotlight' )
-
 yacona.chdir( __dirname )
+
+yacona.addClientModule( 'base', 'resources/css/base.css' )
+yacona.addClientModule( 'init', 'resources/js/init.js' )
+yacona.addClientModule( 'loading', 'resources/img/loading.gif' )
+
 yacona.localAppLoader( 'applications/controller' )
