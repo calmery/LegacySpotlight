0adabe389f75eebacc5100739fe18f34a7470496
calmery <contact@calmery.me>
2017-04-09 17:17:45 +0900

    Update common css

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,3 +1,21 @@
 const socket = io()
 
 socket.emit( 'connection', true )
+
+let loading
+if( document.getElementById( 'loading' ) !== null ){
+    loading = {
+        e: document.getElementById( 'loading' ),
+        show: () => {
+            loading.e.className = 'show fadeIn' 
+        },
+        hide: () => {
+            loading.e.className = 'fadeOut'
+            setTimeout( () => { loading.e.className = 'hide' }, 1000 )
+        }
+    }
+
+    document.addEventListener( 'DOMContentLoaded', () => {
+        loading.hide()
+    }, false )
+}
