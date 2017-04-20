966018554c671e7d83d1557c9d95ed6bfd2f01ec
calmery <contact@calmery.me>
2017-04-21 03:20:29 +0900

    Add comment to index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const yacona = require( 'yacona' )
 
+// Setting
 yacona.setPrefix( 'spotlight' )
 yacona.chdir( __dirname )
 
+// Resources
 yacona.addClientModule( 'base', 'resources/css/base.css' )
 yacona.addClientModule( 'fonts', 'resources/css/font.css' )
 
@@ -15,5 +17,6 @@ yacona.addClientModule( 'init', 'resources/js/init.js' )
 yacona.addClientModule( 'loading', 'resources/img/loading.gif' )
 yacona.addClientModule( 'icon', 'resources/img/icon.ico' )
 
+// Launch
 yacona.localAppLoader( 'applications/api' )
 yacona.localAppLoader( 'applications/controller' )
